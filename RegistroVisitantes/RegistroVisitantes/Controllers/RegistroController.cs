7308240810eda26eb8982ffd96d1f974204e610d
Jeffry Venegas <jeffvene@gmail.com>
2016-04-13 10:37:14 -0600

    Interfaces

---
+++ RegistroVisitantes/RegistroVisitantes/Controllers/RegistroController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistroVisitantes.Controllers
+{
+    public class RegistroController : Controller
+    {
+        // GET: Registro
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Registro/Reservas
+        [HttpGet]
+        public ActionResult Reservas()
+        {
+            return View();
+        }
+
+        // GET: /Registro/Visitantes
+        [HttpGet]
+        public ActionResult Visitantes()
+        {
+            return View();
+        }
+    }
+}
