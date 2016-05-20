6c99f27093a7c0232def3ce49a25c341c7992b81
jeffryvenegas <jeffvene@gmail.com>
2016-05-20 09:01:31 -0600

    Cambios

--- RegistroVisitantes/RegistroVisitantes/Controllers/VisitantesController.cs
+++ RegistroVisitantes/RegistroVisitantes/Controllers/VisitantesController.cs
@@ -309,7 +309,8 @@ namespace RegistroVisitantes.Controllers
                 return HttpNotFound();
             }
             
-            iInfoVisita.ESTADO = false;
+
+            iInfoVisita.ESTADO = !iInfoVisita.ESTADO;
             //db.INFOVISITA.Remove(pREREGISTROCONTACTO);
             BDRegistro.SaveChanges();
 
