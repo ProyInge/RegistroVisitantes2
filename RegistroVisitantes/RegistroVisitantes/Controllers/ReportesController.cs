3e8ffb68c5158432fd09fb4a384a21496c204be2
jeffryvenegas <jeffvene@gmail.com>
2016-06-28 09:42:34 -0600

    Vamo a ver

--- RegistroVisitantes/RegistroVisitantes/Controllers/ReportesController.cs
+++ RegistroVisitantes/RegistroVisitantes/Controllers/ReportesController.cs
@@ -51,9 +51,6 @@ namespace RegistroVisitantes.Controllers
 
             //tabla = db.INFOVISITA.Where(x => String.Equals(x.ID_RESERVACION, reservacion.ID)).OrderBy(x => x.ID_RESERVACION);
             tabla = db.INFOVISITA.OrderBy(x => x.ID_RESERVACION);
-
-            saveExcel(tabla, true, true, true, true, true, true, true, true, true);
-
             int Size_Of_Page = 5;
             int No_Of_Page = (Pagina ?? 1);
             return View(tabla.ToPagedList(No_Of_Page, Size_Of_Page));
@@ -224,75 +221,6 @@ namespace RegistroVisitantes.Controllers
             return View(tabla.ToPagedList(No_Of_Page, Size_Of_Page));
         }
 
-        public DataTable toDataTable(IEnumerable<INFOVISITA> tabla, bool? col1, bool? col2, bool? col3, bool? col4, bool? col5, bool? col6, bool? col7, bool? col8, bool? col9)
-        {
-            var dt = new DataTable();
-            if (col1 != false) { dt.Columns.Add("Compañía", typeof(String)); }
-            if (col2 != false) { dt.Columns.Add("Estación", typeof(String)); }
-            if (col3 != false) { dt.Columns.Add("Reservación", typeof(String)); }
-            if (col4 != false) { dt.Columns.Add("Fecha entrada", typeof(DateTime)); }
-            if (col5 != false) { dt.Columns.Add("Fecha salida", typeof(DateTime)); }
-            if (col6 != false) { dt.Columns.Add("Tipo visitante", typeof(String)); }
-            if (col7 != false) { dt.Columns.Add("Nacionalidad", typeof(String)); }
-            if (col8 != false) { dt.Columns.Add("Nombre completo", typeof(String)); }
-            if (col9 != false) { dt.Columns.Add("Correo electrónico", typeof(String)); }
-            foreach(INFOVISITA iv in tabla)
-            {
-                DataRow newRow = dt.NewRow();
-                if (col1 != false) { newRow["Compañía"] = iv.RESERVACION.ANFITRIONA == "01" ? "OET" : "ESINTRO"; }
-                if (col2 != false) { newRow["Estación"] = iv.RESERVACION.ESTACIONI.NOMBRE; }
-                if (col3 != false) { newRow["Reservación"] = iv.RESERVACION.NUMERO; }
-                if (col4 != false) { newRow["Fecha entrada"] = iv.RESERVACION.ENTRA; }
-                if (col5 != false) { newRow["Fecha salida"] = iv.RESERVACION.SALE; }
-                if (col6 != false) { newRow["Tipo visitante"] = iv.PERSONA.ROL; }
-                if (col7 != false) { newRow["Nacionalidad"] = iv.PERSONA.NACIONALIDADI != null? iv.PERSONA.NACIONALIDADI.GENTILICIO : ""; }
-                if (col8 != false) { newRow["Nombre completo"] = iv.PERSONA.NOMBRE + " " + iv.PERSONA.APELLIDO; }
-                if (col9 != false) { newRow["Correo electrónico"] = iv.PERSONA.EMAIL; }
-                dt.Rows.Add(newRow);
-            }
-            return dt;
-        }
-
-        public void saveExcel(IQueryable<INFOVISITA> t, bool? col1, bool? col2, bool? col3, bool? col4, bool? col5, bool? col6, bool? col7, bool? col8, bool? col9)
-        {
-            String fileName = Server.MapPath("~/Reporte.xlsx");
-            var arch = new FileInfo(fileName);
-            arch.Delete();
-            using (ExcelPackage pck = new ExcelPackage(arch))
-            {
-
-                ExcelWorksheet ws = pck.Workbook.Worksheets.Add("Reporte");
-                IEnumerable<INFOVISITA> tablaenum = t.AsEnumerable<INFOVISITA>();
-                DataTable res = toDataTable(tablaenum, col1, col2, col3, col4, col5, col6, col7, col8, col9);
-                //ver cual celda es la que trae las fechas y cambiar por los numeros
-                ws.Column(5).Style.Numberformat.Format = "yyyy-mm-dd h:mm";
-                ws.Column(4).Style.Numberformat.Format = "yyyy-mm-dd h:mm";
-
-
-                ws.Cells["A1"].LoadFromDataTable(res, true);
-
-               //ver la cantidad de parametros que vienen en true y cambiar la cantCeldas
-                int cantCeldas = 9;
-                int contador = 1;
-                while (contador <= cantCeldas) {
-                    ws.Cells[1,contador].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
-                    ws.Cells[1, contador].Style.Fill.BackgroundColor.SetColor(Color.LightBlue);
-                    ws.Cells[1, contador].Style.Font.Bold = true;
-                    contador++;
-                }
-                ws.Cells.AutoFitColumns();
-
-                pck.Save();
-            }
-        }
-
-        public ActionResult Download()
-        {
-
-            string file = Server.MapPath("~/Reporte.xlsx"); ;
-            string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-            return File(file, contentType, Path.GetFileName(file));
-        }
 
         protected override void Dispose(bool disposing)
         {
